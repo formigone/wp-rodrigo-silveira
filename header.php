4b41a68b0da805a32ffc2df605d27a6f78a6109a
Rodrigo Silveira <rsilveira@deseretdigital.com>
2013-09-09 00:09:05 -0600

    Added responsive top nav and billboard

--- header.php
+++ header.php
@@ -1,60 +1,72 @@
 <!doctype html>
-<html>
+<html lang="eng">
 <head>
+<meta charset="utf-8" />
 <meta name="google-site-verification" content="J4BsImcQAs1UzEhfRNvDOgwWjWS8uNEKAK0UgB2qYSY" />
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Rodrigo Silveira</title>
-  <link rel="stylesheet" type="text/css" href="/css/sliderman.css" />
-  <link href="http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:300" rel="stylesheet" type="text/css">
-  <link href="http://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://formigone.com/pubrecs/icons/rodrigo-silveira-icon-57x57.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://formigone.com/pubrecs/icons/rodrigo-silveira-icon-72x72.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://formigone.com/pubrecs/icons/rodrigo-silveira-icon-114x114.png" />
-  <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s latest posts', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
-  <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
-  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
- <script type="text/javascript" src="<?= bloginfo("template_directory"); ?>/js/sliderman.1.3.7.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono' rel='stylesheet' type='text/css'>
+<link href="//fonts.googleapis.com/css?family=Yanone+Kaffeesatz:300" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo("template_url"); ?>/css/bootstrap-3.0.0.min.css" />
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo("template_url"); ?>/css/style.css" />
+
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://formigone.com/pubrecs/icons/rodrigo-silveira-icon-57x57.png" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://formigone.com/pubrecs/icons/rodrigo-silveira-icon-72x72.png" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://formigone.com/pubrecs/icons/rodrigo-silveira-icon-114x114.png" />
+
+<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s latest posts', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<script src="<?php bloginfo("template_url"); ?>/js/jquery-2.0.3.min.js"></script>
+<script src="<?php bloginfo("template_url"); ?>/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<div id="header">
-  <div class="w980">
-    <a href="/" id="homelink"><img src="<?php bloginfo('template_directory');?>/img/logo.png" alt="Rodrigo Silveira"/></a>
-<script>
-var initLoadTime = (new Date()).getTime();
-function howLong(){
-  var now = (new Date()).getTime();
-  return (now - initLoadTime) / 1000;
-}
+	<nav id="v2-nav" class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+			   <a href="/" id="homelink">
+			      <img src="<?php bloginfo('template_directory');?>/img/logo.png" alt="Rodrigo Silveira" />
+		      </a>
 
-function trackNav(nav, link){
-  return _gaq.push(['_trackEvent', nav, link, parseInt(howLong())]);
-}
-</script>
-    <ul id="nav">
-      <li><a href="/coding/" id="navLinkCoding">/coding</a></li>
-      <li><a href="/design/" id="navLinkDesign">/design</a></li>
-      <li><a href="/rodrigo-silveira/" id="navLinkAbout">/about_me</a></li>
-      <li><a href="/blog" id="navLinkBlog">/blog</a></li>
-      <li><a href="/contact-me/" id="navLinkContact">/contact</a></li>
-    </ul>
-  </div>
-</div>
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span> <span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
 
-<?php if(is_front_page()):?>
-<div id="billboard">
-  <div class="w980">
-    <div class="w650" id="selfpromo">
-      <p><span style="font-size: 4.50em;color:#fff;">I am a web application developer</span><br/> <span style="font-size: 2.10em"><span style="color:#fff">specialized in coding</span> awesome stuff.</span><br/> <span style="font-size: 2.10em">I'm also deeply involved in <span style="color:#fff">native mobile development</span>, <span style="color:#fff">game development</span>, and <span style="color:#fff">open source projects</span>.</span></p>
-    </div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div id="v2-nav-links" class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/coding/">/coding</a></li>
+					<li><a href="/design/">/design</a></li>
+					<li><a href="/rodrigo-silveira/">/about_me</a></li>
+					<li><a href="/blog">/blog</a></li>
+					<li><a href="/contact-me/">/contact</a></li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
 
-  </div>
-</div>
+		</div>
+	</nav>
+	<div class="v2-main">
+	<?php if(is_front_page()):?>
+   <div id="billboard" class="container">
+		<div class="col-md-9">
+				<p>I am a web application developer specialized in coding awesome stuff. I'm also deeply involved in native mobile development, game development, and open source projects.
+				</p>
+		</div>
+		</div>
+	</div>
 
 <?php endif;?>
+	</div>
 
 <div id="main">
-  <div class="w980">
-    <div class="w560boxit10">
+		<div class="w980">
+			<div class="w560boxit10">
