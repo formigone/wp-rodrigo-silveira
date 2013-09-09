8df0fb304974b6a37d8ff5c4209dba1cbd5a1508
Rodrigo Silveira <rsilveira@deseretdigital.com>
2013-09-08 23:10:19 -0600

    Added theme screenshot

--- index.php
+++ index.php
@@ -3,51 +3,67 @@
 
 
 <?php
-/*****************************************
+/**
+ * ***************************************
  * what will appear on the blog main page
- *****************************************/
+ * ***************************************
+ */
 ?>
 
 <h1 class="coolTitle">\ Blog</h1>
-<p>This is where I post about web development, general programming, desing, graphics stuff, computers in general, and occasionally about less important things, such as life in the real world.</p>
-<hr/>
+<p>This is where I post about web development, general programming,
+	desing, graphics stuff, computers in general, and occasionally about
+	less important things, such as life in the real world.</p>
+<hr />
 
 <ul class="recent_posts">
 <?php
-   $recent_posts = wp_get_recent_posts( array('numberposts' => 23) );
-      foreach( $recent_posts as $recent ):?>
+$recent_posts = wp_get_recent_posts(array(
+   'numberposts' => 23
+));
+foreach ($recent_posts as $recent) :
+   ?>
 
 <li>
-   <h2 class="coolTitle"><a href="<?= get_permalink($recent["ID"]);?>" title="<?php $recent["post_title"];?>"><?= $recent["post_title"]; ?></a></h2>
-   <div class="post_thumbnail">
-      <a href="<?= get_permalink($recent["ID"]);?>" alt="<?php $recent["post_title"];?>">
+		<h2 class="coolTitle">
+			<a href="<?= get_permalink($recent["ID"]);?>"
+				title="<?php $recent["post_title"];?>"><?= $recent["post_title"]; ?></a>
+		</h2>
+		<div class="post_thumbnail">
+			<a href="<?= get_permalink($recent["ID"]);?>"
+				alt="<?php $recent["post_title"];?>">
          <?= get_the_post_thumbnail( $recent["ID"], 'thumbnail'); ?>
       </a>
-   </div>
+		</div>
 
-   <div class="post_summary">
-      <p class="post_author" style="margin: 0 0 10px;"><a href="/rodrigo-silveira">Rodrigo Silveira</a> on <?= the_modified_date('M j, Y');?> @ <?= the_modified_date('g:i a');?></p>
+		<div class="post_summary">
+			<p class="post_author" style="margin: 0 0 10px;">
+				<a href="/rodrigo-silveira">Rodrigo Silveira</a> on <?= the_modified_date('M j, Y');?> @ <?= the_modified_date('g:i a');?></p>
 
 <?php
 
-//
-// generate excerpt using first x words of post
-//
+   //
+   // generate excerpt using first x words of post
+   //
 
-// display the first 75 words of post
-/***********************************************************************
-$words = 70;
-$preview = preg_split('/[\s]+/',  $recent['post_excerpt'], $words + 1);
-$preview = array_slice($preview, 0, $words);
-$preview = join(' ', $preview);
-***********************************************************************/
-?>
+   // display the first 75 words of post
+   /**
+    * *********************************************************************
+    * $words = 70;
+    * $preview = preg_split('/[\s]+/', $recent['post_excerpt'], $words + 1);
+    * $preview = array_slice($preview, 0, $words);
+    * $preview = join(' ', $preview);
+    * *********************************************************************
+    */
+   ?>
 
-         <p class="post_summary"><?= $recent['post_excerpt']; ?> <strong><a href="<?= get_permalink($recent["ID"]); ?>">Read More</a></strong></p>
-   </div>
-<div style="height:1px;margin:10px 0;clear:both"></div>
-<hr/>
-</li>
+         <p class="post_summary"><?= $recent['post_excerpt']; ?> <strong><a
+					href="<?= get_permalink($recent["ID"]); ?>">Read More</a></strong>
+			</p>
+		</div>
+		<div style="height: 1px; margin: 10px 0; clear: both"></div>
+		<hr />
+	</li>
       <?php endforeach; ?>
 
 
--- single.php
+++ single.php
@@ -1,28 +1,34 @@
 <?php get_header();?>
 
 <h1 class="coolTitle">\ <?php the_title(); ?></h1>
-<p>Author: <a href="https://plus.google.com/103737161295645708507?rel=author" rel="publisher">Rodrigo Silveira</a></p>
+<p>
+	Author: <a
+		href="https://plus.google.com/103737161295645708507?rel=author"
+		rel="publisher">Rodrigo Silveira</a>
+</p>
 
-<div style="margin: 5px 0; padding: 5px 0; border-top: 1px solid #aaa; border-bottom: 1px solid #aaa;">
-<!-- AddThis Button BEGIN -->
-<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-<a class="addthis_button_preferred_11"></a>
-<a class="addthis_button_preferred_1"></a>
-<a class="addthis_button_preferred_2"></a>
-<a class="addthis_button_preferred_6"></a>
-<a class="addthis_button_preferred_8"></a>
-<a class="addthis_button_preferred_10"></a>
-<a class="addthis_button_preferred_13"></a>
-<a class="addthis_button_preferred_14"></a>
-<a class="addthis_button_preferred_3"></a>
-<a class="addthis_button_preferred_4"></a>
-<a class="addthis_button_preferred_5"></a>
-<a class="addthis_button_compact"></a>
-<a class="addthis_counter addthis_bubble_style"></a>
-</div>
-<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50c0f5850d6a6558"></script>
-<!-- AddThis Button END -->
+<div
+	style="margin: 5px 0; padding: 5px 0; border-top: 1px solid #aaa; border-bottom: 1px solid #aaa;">
+	<!-- AddThis Button BEGIN -->
+	<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+		<a class="addthis_button_preferred_11"></a> <a
+			class="addthis_button_preferred_1"></a> <a
+			class="addthis_button_preferred_2"></a> <a
+			class="addthis_button_preferred_6"></a> <a
+			class="addthis_button_preferred_8"></a> <a
+			class="addthis_button_preferred_10"></a> <a
+			class="addthis_button_preferred_13"></a> <a
+			class="addthis_button_preferred_14"></a> <a
+			class="addthis_button_preferred_3"></a> <a
+			class="addthis_button_preferred_4"></a> <a
+			class="addthis_button_preferred_5"></a> <a
+			class="addthis_button_compact"></a> <a
+			class="addthis_counter addthis_bubble_style"></a>
+	</div>
+	<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+	<script type="text/javascript"
+		src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50c0f5850d6a6558"></script>
+	<!-- AddThis Button END -->
 </div>
 
 
@@ -32,28 +38,33 @@
 <?php endwhile;?>
 <?php endif;?>
 
-<hr/>
-<?php 
-/****************************************************
-* Related Posts
-*****************************************************/
-if(function_exists('echo_ald_crp')) echo_ald_crp();
+<hr />
+<?php
+/**
+ * **************************************************
+ * Related Posts
+ * ****************************************************
+ */
+if (function_exists('echo_ald_crp'))
+   echo_ald_crp();
 ?>
 
-<hr/>
+<hr />
 
 
-<?php 
+<?php
 //
 // TODO: add comments.php
 //
 ?>
 
 <?php
-/****************************************************
-* Comments Form
-*****************************************************/
-//comment_form(array('must_log_in' => false, 'comment_notes_after' => ''));
+/**
+ * **************************************************
+ * Comments Form
+ * ****************************************************
+ */
+// comment_form(array('must_log_in' => false, 'comment_notes_after' => ''));
 ?>
 <?php //comments_template(); ?>
 
@@ -66,14 +77,16 @@ if(function_exists('echo_ald_crp')) echo_ald_crp();
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=131034187065096";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<div class="fb-comments" data-href="<?= the_permalink(); ?>" data-width="540" data-num-posts="23"></div>
+<div class="fb-comments" data-href="<?= the_permalink(); ?>"
+	data-width="540" data-num-posts="23"></div>
 
 <?php get_sidebar();?>
 
 <?php get_footer();?>
 
 <!-- AddThis Welcome BEGIN -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50c0f5850d6a6558"></script>
+<script type="text/javascript"
+	src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50c0f5850d6a6558"></script>
 <script type='text/javascript'>
 addthis.bar.initialize({'default':{
 	"backgroundColor": "#000000",
