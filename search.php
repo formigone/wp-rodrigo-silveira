d2e959b5c9281d2fcb361f0efbbe7702cc7caa4d
Rodrigo Silveira <rsilveira@deseretdigital.com>
2013-11-22 16:42:43 -0700

    Added the rest of the pages (articles, tags, archives, search, etc.). Now I just need to style the side bar, as well as native WP widgets. Also need to clean up each page's template (guts)

--- search.php
+++ search.php
@@ -1,82 +1,90 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-<h1 class="coolTitle">\Search Results</h1>
+<div class="row">
+   <div class="container">
+      <div class="col-md-9">
+         <div class="panel panel-default">
+            <div class="panel-body">
+               <h1 class="coolTitle">Search Results</h1>
 
-<?php ///////////////////////////////////////////////////// ?>
+               <?php if (have_posts()) : ?>
 
-<?php if ( have_posts() ) : ?>
- 
-<h3 class="coolTitle"><?php _e( 'Search Results for: ', 'your-theme' ); ?><span><?php the_search_query(); ?></span></h3>
- 
-<?php global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
-	<div id="nav-above" class="navigation">
-		<div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older posts', 'your-theme' )) ?></div>
-		<div class="nav-next"><?php previous_posts_link(__( 'Newer posts <span class="meta-nav">&raquo;</span>', 'your-theme' )) ?></div>
-	</div><!-- #nav-above -->
-<?php } ?>                            
+                  <h3><?php _e('Search Results for: ', 'your-theme'); ?><span><?php the_search_query(); ?></span></h3>
+
+                  <?php
+                  global $wp_query;
+                  $total_pages = $wp_query->max_num_pages;
+                  ?>
+                  <?php if ($total_pages > 1): ?>
+                     <div id="nav-above" class="navigation">
+                        <div
+                           class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'your-theme')) ?></div>
+                        <div
+                           class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>', 'your-theme')) ?></div>
+                     </div><!-- #nav-above -->
+                  <?php endif; /* if total_pages > 1 */ ?>
 
 
-<ul class="recent_posts">
- 
-<?php while ( have_posts() ) : the_post() ?>
- 
+                  <ul class="recent_posts">
 
-<li>
+                     <?php while (have_posts()) : the_post() ?>
+                        <li>
+                           <?php if (get_the_post_thumbnail($recent["ID"], 'thumbnail') !== '') : ?>
+                              <div class="post_thumbnail">
+                                 <a href="<?= get_permalink($recent["ID"]); ?>" alt="<?php $recent["post_title"]; ?>">
+                                    <?= get_the_post_thumbnail($recent["ID"], 'thumbnail'); ?>
+                                 </a>
+                              </div>
 
-  <?php if(get_the_post_thumbnail( $recent["ID"], 'thumbnail') !== '') :?>
+                           <?php else: /* no thumbnail */ ?>
+                           <?php endif; /* if has_thumbnail */ ?>
 
-   <div class="post_thumbnail">
-      <a href="<?= get_permalink($recent["ID"]);?>" alt="<?php $recent["post_title"];?>">
-         <?= get_the_post_thumbnail( $recent["ID"], 'thumbnail'); ?>
-      </a>
+                           <a href="<?= get_permalink($recent["ID"]); ?>" title="<?php $recent["post_title"]; ?>">
+                              <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+                              <p class="post_author">
+                                 <a href="/rodrigo-silveira">Rodrigo Silveira</a> on <?= the_modified_date('M j, Y'); ?>
+                                 @ <?= the_modified_date('g:i a'); ?>
+                              </p>
+
+                              <p class="post_read_more"><a href="<?= get_permalink($recent["ID"]); ?>">Read More</a></p>
+
+                              <div style="height:1px;margin:10px 0;clear:both"></div>
+                              <hr/>
+                        </li>
+
+                     <?php endwhile; /* while have_posts */ ?>
+                  </ul>
+                  <?php
+                  global $wp_query;
+                  $total_pages = $wp_query->max_num_pages;
+                  ?>
+                  <?php if ($total_pages > 1): ?>
+                     <div id="nav-below" class="navigation">
+                        <div
+                           class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'your-theme')) ?></div>
+                        <div
+                           class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>', 'your-theme')) ?></div>
+                     </div><!-- #nav-below -->
+
+                  <?php endif; /* total_pages > 1 */ ?>
+
+               <?php else: /* no posts */ ?>
+
+                  <h2 class="coolTitle"><?php _e('Nothing Found', 'your-theme') ?></h2>
+
+                  <div class="entry-content">
+                     <p><?php _e('Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'your-theme'); ?></p>
+                     <?php get_search_form(); ?>
+                  </div>
+
+               <?php endif; /* if have_posts */ ?>
+            </div>
+         </div>
+      </div>
+
+      <div class="col-md-3">
+         <?php get_sidebar(); ?>
+      </div>
    </div>
-
-   <div class="post_summary">
-
-   <?php else: ?>
-	<div class="post_summary" style="width:100%">
-   <?php endif; ?>
-
-      <a href="<?= get_permalink($recent["ID"]);?>" title="<?php $recent["post_title"];?>">
-         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-         <p class="post_author"><a href="/rodrigo-silveira">Rodrigo Silveira</a> on <?= the_modified_date('M j, Y');?> @ <?= the_modified_date('g:i a');?></p>
-
-<?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
-	<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
-
-         <p class="post_read_more"><a href="<?= get_permalink($recent["ID"]); ?>">Read More</a></p>
-   </div>
-<div style="height:1px;margin:10px 0;clear:both"></div>
-<hr/>
-</li>
-
-
-<?php endwhile; ?>
-
-</ul>
- 
-<?php global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
-                <div id="nav-below" class="navigation">
-                    <div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older posts', 'your-theme' )) ?></div>
-                    <div class="nav-next"><?php previous_posts_link(__( 'Newer posts <span class="meta-nav">&raquo;</span>', 'your-theme' )) ?></div>
-                </div><!-- #nav-below -->
-
-<?php } ?>            
- 
-<?php else : ?>
- 
-                <div id="post-0" class="post no-results not-found">
-                    <h2 class="coolTitle"><?php _e( 'Nothing Found', 'your-theme' ) ?></h2>
-                    <div class="entry-content">
-                        <p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'your-theme' ); ?></p>
-    <?php get_search_form(); ?>
-                    </div><!-- .entry-content -->
-                </div>
- 
-<?php endif; ?>
-
-<?php ///////////////////////////////////////////////////// ?>
-
-<?php get_sidebar();?>
-
-<?php get_footer();?>
+</div>
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,4 @@
 <?php
-
 if(is_front_page())
 {
 	if( is_active_sidebar( 'homepage-sidebar1' ) )
