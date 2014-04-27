0d2274d6b4923240e01de00037869f5907f080d8
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-04-27 02:22:59 -0600

    Got blog listing page

--- index.php
+++ index.php
@@ -1,53 +1,57 @@
 <?php get_header(); ?>
 
-<?php $recent_posts = wp_get_recent_posts(array('numberposts' => 50)); ?>
-<ul class="grid effect-<?= rand(1, 8); ?>" id="grid">
-   <?php foreach ($recent_posts as $recent): ?>
-      <li>
-         <div class="gridCard">
-            <div class="meBlogTile">
-               <div class="meBlogTileHeader">
-                  <a href="<?= get_permalink($recent["ID"]); ?>">
-                     <?= get_the_post_thumbnail($recent["ID"], 'thumbnail'); ?>
+<div class="container pt">
+   <div class="row mt">
+      <div class="col-md-9">
+         <?php $recent_posts = wp_get_recent_posts(array('numberposts' => 50)); ?>
+         <?php foreach ($recent_posts as $recent): ?>
+
+            <div class="col-lg-8 col-lg-offset-2">
+               <p>
+                  <img src="<?php bloginfo("template_url"); ?>/img/rodrigo-silveira.jpg" width="50px" height="50px"
+                       class="img-circle">
+                  <ba>
+                     <a
+                        href="https://plus.google.com/103737161295645708507?rel=author"
+                        rel="publisher" style="color: #333">Rodrigo Silveira</a>
+                  </ba>
+               </p>
+               <p>
+                  <bd><?= the_modified_date('M j, Y'); ?></bd>
+               </p>
+               <h4><?= $recent["post_title"]; ?></h4>
+
+               <div class="media">
+                  <a class="pull-left" href="<?= get_permalink($recent["ID"]); ?>">
+                     <?= preg_replace('/http:\/\/rodrigo-silveira.com/', '', get_the_post_thumbnail($recent["ID"], 'thumbnail', array('class' => 'media-object'))); ?>
                   </a>
-               </div>
+                  <div class="media-body">
+                     <p><?= $recent['post_excerpt']; ?></p>
 
-               <div class="meBlogTileBody">
-                  <h3>
-                     <a href="<?= get_permalink($recent["ID"]); ?>">
-                        <?= $recent["post_title"]; ?>
-                     </a>
-                  </h3>
-
-                  <p>
-                     <small>
-                        <a
-                           href="https://plus.google.com/103737161295645708507?rel=author"
-                           rel="publisher">Rodrigo Silveira</a>, <?= the_modified_date('M j, Y'); ?>
-                     </small>
-                  </p>
-
-                  <p>
-                     <?= $recent['post_excerpt']; ?>
-                  </p>
-
-                  <p>
-                     <?php $posttags = wp_get_post_tags($recent["ID"]); ?>
-                     <?php if ($posttags): ?>
-                        <?php foreach ($posttags as $_tag): ?>
-                           <span class="label label-default">
-                                          <a href="<?= get_tag_link($_tag->term_id); ?>"><?= $_tag->name; ?></a>
+                     <p>
+                        <?php $posttags = wp_get_post_tags($recent["ID"]); ?>
+                        <?php if ($posttags): ?>
+                           <?php foreach ($posttags as $_tag): ?>
+                              <span class="label label-default">
+                                          <a href="<?= get_tag_link($_tag->term_id); ?>"
+                                             style="color: #fff"><?= $_tag->name; ?></a>
                                        </span>&nbsp;
-                        <?php endforeach; ?>
-                     <?php endif; ?>
-                  </p>
-
-                  <div class="clearfix"></div>
+                           <?php endforeach; ?>
+                        <?php endif; ?>
+                     </p>
+                  </div>
                </div>
+
+               <p><a href="<?= get_permalink($recent["ID"]); ?>">Continue Reading...</a></p>
+               <hr/>
+               <p><br/></p>
             </div>
-         </div>
-      </li>
-   <?php endforeach; ?>
-</ul>
-<?php get_sidebar(); ?>
+         <?php endforeach; ?>
+      </div>
+      <div class="col-md-3 well">
+         <?php get_sidebar(); ?>
+      </div>
+   </div>
+</div>
+
 <?php get_footer(); ?>
